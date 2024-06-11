0269eed18ea0ae8d4a868c94eb1357ef0e1a6a7e
Ciro Watanabe <cw.artiga@gmail.com>
2024-06-11 15:48:13 -0300

    project 1

---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,18 @@
+import { PostCard } from '../PostCard';
+import './styles.css';
+
+export const Posts = ({posts}) => {
+  return (
+    <div className="posts">
+      {posts.map((post) => (
+        <PostCard
+          key={post.id}
+          id={post}
+          cover={post.cover}
+          title={post.title}
+          body={post.body}
+        />
+      ))}
+    </div>
+  );
+};
